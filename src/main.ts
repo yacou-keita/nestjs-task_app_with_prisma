3528ac2cc86bd12a3f647f978a7cce96530cf3dd
Yacou Keita <yacoukeita0212@gmail.com>
2024-09-09 17:25:16 +0000

    add Swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: [] })
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  const config = new DocumentBuilder()
+    .setTitle('User Task app')
+    .setDescription('handler user tasks')
+    .setVersion('1.0')
+    .addTag('User Task')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
